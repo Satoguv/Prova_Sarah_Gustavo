15b55c66e0e7bcc47a66b504718dc51429ccfafe
Satoguv <guscafer@gmail.com>
2024-10-29 09:55:12 -0300

    Adicionado o link para acessar os cadastros

--- cadastros.php
+++ cadastros.php
@@ -6,8 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>SARAH & GUSTAVO</h1>
-    <img src="Imagens/pizza.png">
-    <a href="cadastros.php">Acessar os cadastros</a>
+    
 </body>
 </html>
